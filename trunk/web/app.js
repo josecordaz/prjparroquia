c481e1171ecb295fd7aefdcf1893fc33950ba1fc
sonykarl <sonykarl@38d72475-e792-8ac8-9109-b21f5677cea6>
2012-11-08 19:59:51 +0000

    Se arreglaron los encabezados, se trabaja en el store

--- trunk/web/app.js
+++ trunk/web/app.js
@@ -89,94 +89,166 @@ Ext.onReady(function(){
     // to use for the editor at each column.
     var grid = Ext.create('Ext.grid.Panel', {
         store: store,
-        columns: [{
-            header: 'Name',
-            dataIndex: 'name',
-            flex: 1,
-            editor: {
-                // defaults to textfield if no xtype is supplied
-                allowBlank: false
+        columns: [
+            {
+                text:'Hombre',
+                columns:[
+                    {
+                        header: 'Apellido Paterno',
+                        dataIndex: 'name',
+                        width: 90,
+                        editor: {
+                            // defaults to textfield if no xtype is supplied
+                            allowBlank: false
+                        }
+                    },
+                    {
+                        header: 'Apellido Materno',
+                        dataIndex: 'email',
+                        width: 90,
+                        editor: {
+                            allowBlank: false,
+                            vtype: 'email'
+                        }
+                    },
+                    {
+                        xtype: 'datecolumn',
+                        header: 'Nombre',
+                        dataIndex: 'start',
+                        width: 90,
+                        editor: {
+                            xtype: 'datefield',
+                            allowBlank: false,
+                            format: 'm/d/Y',
+                            minValue: '01/01/2006',
+                            minText: 'Cannot have a start date before the company existed!',
+                            maxValue: Ext.Date.format(new Date(), 'm/d/Y')
+                        }
+                    }
+                ]
             }
-        }, {
-            header: 'Email',
-            dataIndex: 'email',
-            width: 160,
-            editor: {
-                allowBlank: false,
-                vtype: 'email'
+            ,
+            {
+                text:'Mujer',
+                columns:[
+                    {
+                        xtype: 'numbercolumn',
+                        header: 'Apellido Paterno',
+                        dataIndex: 'salary',
+                        format: 'EUR 0',
+                        width: 90,
+                        editor: {
+                            xtype: 'numberfield',
+                            allowBlank: false,
+                            minValue: 1,
+                            maxValue: 150000
+                        }
+                    }, {
+                        xtype: 'checkcolumn',
+                        header: 'Apellido Materno',
+                        dataIndex: 'active',
+                        width: 90,
+                        editor: {
+                            xtype: 'checkbox',
+                            cls: 'x-grid-checkheader-editor'
+                        }
+                    }, {
+                        xtype: 'checkcolumn',
+                        header: 'Nombre',
+                        dataIndex: 'active',
+                        width: 90,
+                        editor: {
+                            xtype: 'checkbox',
+                            cls: 'x-grid-checkheader-editor'
+                        }
+                    }
+                ]
+            },
+            {
+                xtype: 'numbercolumn',
+                header: 'Unk_0',
+                dataIndex: 'salary',
+                format: 'EUR 0',
+                width: 50,
+                editor: {
+                    xtype: 'numberfield',
+                    allowBlank: false,
+                    minValue: 1,
+                    maxValue: 150000
+                }
+            }, {
+                xtype: 'checkcolumn',
+                header: 'Unk_1',
+                dataIndex: 'active',
+                width: 50,
+                editor: {
+                    xtype: 'checkbox',
+                    cls: 'x-grid-checkheader-editor'
+                }
+            }, {
+                xtype: 'checkcolumn',
+                header: 'Unk_2',
+                dataIndex: 'active',
+                width: 50,
+                editor: {
+                    xtype: 'checkbox',
+                    cls: 'x-grid-checkheader-editor'
+                }
             }
-        }, {
-            xtype: 'datecolumn',
-            header: 'Start Date',
-            dataIndex: 'start',
-            width: 90,
-            editor: {
-                xtype: 'datefield',
-                allowBlank: false,
-                format: 'm/d/Y',
-                minValue: '01/01/2006',
-                minText: 'Cannot have a start date before the company existed!',
-                maxValue: Ext.Date.format(new Date(), 'm/d/Y')
-            }
-        }, {
-            xtype: 'numbercolumn',
-            header: 'Salary',
-            dataIndex: 'salary',
-            format: 'EUR 0',
-            width: 90,
-            editor: {
-                xtype: 'numberfield',
-                allowBlank: false,
-                minValue: 1,
-                maxValue: 150000
-            }
-        }, {
-            xtype: 'checkcolumn',
-            header: 'Active?',
-            dataIndex: 'active',
-            width: 60,
-            editor: {
-                xtype: 'checkbox',
-                cls: 'x-grid-checkheader-editor'
-            }
-        }],
+        ],
         renderTo: 'editor-grid',
-        width: 600,
+        width: 1000,
         height: 400,
-        title: 'Employee Salaries',
+        title: 'Matrimonios',
         frame: true,
-        tbar: [{
-            text: 'Add Employee',
-            iconCls: 'agregar-matrimonio',
-            handler : function() {
-                rowEditing.cancelEdit();
+        tbar: [
+            {
+                text: 'Agregar',
+                iconCls: 'agregar-matrimonio',
+                handler : function() {
+                    rowEditing.cancelEdit();
 
-                // Create a model instance
-                var r = Ext.create('Employee', {
-                    name: 'New Guy',
-                    email: 'new@sencha-test.com',
-                    start: Ext.Date.clearTime(new Date()),
-                    salary: 50000,
-                    active: true
-                });
+                    // Create a model instance
+                    var r = Ext.create('Employee', {
+                        name: 'New Guy',
+                        email: 'new@sencha-test.com',
+                        start: Ext.Date.clearTime(new Date()),
+                        salary: 50000,
+                        active: true
+                    });
 
-                store.insert(0, r);
-                rowEditing.startEdit(0, 0);
-            }
-        }, {
-            itemId: 'removeEmployee',
-            text: 'Remove Employee',
-            iconCls: 'employee-remove',
-            handler: function() {
-                var sm = grid.getSelectionModel();
-                rowEditing.cancelEdit();
-                store.remove(sm.getSelection());
-                if (store.getCount() > 0) {
-                    sm.select(0);
+                    store.insert(0, r);
+                    rowEditing.startEdit(0, 0);
                 }
             },
-            disabled: true
-        }],
+            {
+                itemId: 'removeEmployee',
+                text: 'Eliminar',
+                iconCls: 'eliminar-matrimonio',
+                handler: function() {
+                    var sm = grid.getSelectionModel();
+                    rowEditing.cancelEdit();
+                    store.remove(sm.getSelection());
+                    if (store.getCount() > 0) {
+                        sm.select(0);
+                    }
+                },
+                disabled: true
+            },
+            {
+                itemId: 'importarPdf',
+                text: 'Importar a PDF',
+                iconCls: 'crear-pdf',
+                handler: function() {
+//                    var sm = grid.getSelectionModel();
+//                    rowEditing.cancelEdit();
+//                    store.remove(sm.getSelection());
+//                    if (store.getCount() > 0) {
+//                        sm.select(0);
+//                    }
+                }
+            }
+        ],
         plugins: [rowEditing],
         listeners: {
             'selectionchange': function(view, records) {
